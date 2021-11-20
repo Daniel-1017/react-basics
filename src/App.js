c7ba5da54118c0809146b62f83a1263dd7b5b9ae
Daniel <frimudaniel17@gmail.com>
2021-11-20 11:17:24 +0100

    add new item

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
         {
             id: 'e3',
             title: 'Car Insurance',
@@ -22,17 +22,24 @@ function App() {
             amount: 450,
             date: new Date(2021, 5, 12),
         },
+        {
+            id: 'e5',
+            title: 'Car',
+            amount: 10000,
+            date: new Date(2016, 2, 16),
+        },
     ];
 
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} onClick={}/>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+            <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date}/>
+        </div>
+    );
 }
 
 export default App;
