faccb575f5db94d35dbfe0a26fd39db965097c40
Daniel Frimu <frimudaniel17@gmail.com>
2021-11-20 10:49:41 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// index.js is the first file to be executed
+// source, static/js, here we find the whole react source code and the hole react dom library
+
+// a component in react is just a js function
