6dbc1eb4ed509da723dc856d65239e9b96d1a3ce
Frimu Daniel <frimudaniel17@gmail.com>
2021-11-28 15:02:26 +0100

    49

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
+/*
+If you have data which might change and where changes to that data should be reflected on the user interface
+then you need state.
+*/
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+
     const clickHandler = () => {
-        console.log('Clicked!!')
+        setTitle('Updated!');
     }
     return (
         <Card className="expense-item">
             <ExpenseDate  date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Click here</button>
