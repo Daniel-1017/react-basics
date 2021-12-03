f629d6b5c71efc5a53779834e5f2708e87817f63
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-03 17:29:07 +0100

    67

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,11 +16,17 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No expense found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+                {expensesContent}
             </Card>
         </div>
     )
