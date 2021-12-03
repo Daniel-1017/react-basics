0875851b84910175b35060e2f77335e00bd487e1
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-03 22:04:43 +0100

    71

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,11 +9,8 @@ import Card from "../UI/Card";
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
+    const [title] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle('Updated!');
-    }
     return (
         <Card className="expense-item">
             <ExpenseDate  date={props.date}/>
@@ -21,7 +18,6 @@ function ExpenseItem(props) {
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            {/*<button onClick={clickHandler}>Click here</button>*/}
         </Card>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from './ExpensesList';
 import './Expenses.css'
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -20,6 +21,7 @@ function Expenses(props) {
         <li>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </li>
